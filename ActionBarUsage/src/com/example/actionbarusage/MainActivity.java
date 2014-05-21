bfb9fef6e622d17ea98c6ed68c3a21e98945ff7a
hjeong <Felinus.jeong@windriver.com>
2014-05-21 18:54:08 +0900

    ActionBarUsage_finish

--- ActionBarUsage/src/com/example/actionbarusage/MainActivity.java
+++ ActionBarUsage/src/com/example/actionbarusage/MainActivity.java
@@ -2,16 +2,16 @@ package com.example.actionbarusage;
 
 
 
-import android.annotation.SuppressLint;
+
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.SearchView;
 import android.widget.SearchView.OnQueryTextListener;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -29,6 +29,7 @@ public class MainActivity extends Activity implements OnQueryTextListener
     {
         super.onCreate(savedInstanceState);
         mSearchText = new TextView(this);
+        mSearchText.setText("Hi~Android");
         setContentView(mSearchText);
     }
     
@@ -37,9 +38,13 @@ public class MainActivity extends Activity implements OnQueryTextListener
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// TODO Auto-generated method stub
 		MenuInflater inflater = getMenuInflater();
+		Log.e("MenuInflater",""+inflater);
+		Log.e("before_menu",""+menu);
+
 		inflater.inflate(R.menu.actions, menu);
-		SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-		searchView.setOnQueryTextListener(this);
+		Log.e("after_menu",""+menu);
+	//	SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+	//	searchView.setOnQueryTextListener(this);
 		return true;
 	}
 
@@ -47,36 +52,45 @@ public class MainActivity extends Activity implements OnQueryTextListener
 	@Override
 	public boolean onPrepareOptionsMenu(Menu menu) {
 		// TODO Auto-generated method stub
+		Log.e("onPrepareOptionsMenu: ","onPrepareOptionsMenu");
 		if(mSortMode !=-1){
+			Log.e("onPrepare_mSortMode: ",""+mSortMode);
 			Drawable icon = menu.findItem(mSortMode).getIcon();
-			menu.findItem(R.id.action_sort).setIcon(icon);
+			menu.findItem(R.id.action_sort).setIcon(icon);// 여기서 아이콘 모양을 변화시킨다.
 		}
 		return super.onPrepareOptionsMenu(menu);
+		//return false; false이면 메뉴가 보여지지 않는다.
 	}
 
-	public void onSort(MenuItem item) {
-		mSortMode = item.getItemId();
-		invalidateOptionsMenu();
-	}
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// TODO Auto-generated method stub
+		
 		Toast.makeText(this, "Selected Item: " + item.getTitle(), Toast.LENGTH_SHORT).show();
+		Log.e("onOptionsItemSelected: ",""+mSortMode);
 		return true;
 	}
-
+	
+	public void onSort(MenuItem item) {
+		Log.e("before_onSort",""+mSortMode);
+		mSortMode = item.getItemId();
+		Log.e("after_onSort",""+mSortMode);
+		invalidateOptionsMenu();
+	}
+	// SearchView를 사용하는 예제는 ApiDemos/src/com/example/android/apis/view 에서 Search예제를 참고!!!!
 
 	@Override
 	public boolean onQueryTextChange(String newText) {
 		// TODO Auto-generated method stub
 		newText = newText.isEmpty() ?"": "Query so far: " + newText;
 		mSearchText.setText(newText);
-		return false;
+		return true;
 	}
 	@Override
 	public boolean onQueryTextSubmit(String query) {
 		// TODO Auto-generated method stub
 		Toast.makeText(this, "Searching for: " + query + "....", Toast.LENGTH_SHORT).show();
-		return false;
+		return true;
 	}
 }

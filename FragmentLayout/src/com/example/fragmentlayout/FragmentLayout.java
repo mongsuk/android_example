305eb024ff3d5e22ca3ab06e59c37e38919d753a
hjeong <Felinus.jeong@windriver.com>
2014-05-27 20:01:38 +0900

    FragmentLayout

---
+++ FragmentLayout/src/com/example/fragmentlayout/FragmentLayout.java
@@ -0,0 +1,57 @@
+package com.example.fragmentlayout;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.ListFragment;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.app.Fragment;
+
+@SuppressLint("NewApi")
+public class FragmentLayout extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_layout);
+    }
+    
+    @SuppressLint("NewApi")
+	public static class DetailsActivity extends Activity {
+    	protected void onCreate(Bundle savedInstanceState) {
+    		super.onCreate(savedInstanceState);
+    		
+    		if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+    			finish();
+    			return;
+    		}
+    		
+    		if(savedInstanceState == null) {
+    			DetailsFragment details = new DetailsFragment();
+    			details.setArguments(getIntent().getExtras());
+    			getFragmentManager().beginTransaction().add(android.R.id.content, details).commit();
+    		}
+    	}
+    }
+    
+    public static class TitleFragment extends ListFragment {
+    	boolean mDualPane;
+    	int mCurCheckPosition =0;
+    	
+    	public void onActivityCreated(Bundle savedInstatnceState) {
+    		super.onActivityCreated(savedInstatnceState);
+    		
+    		setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.sample_list_item))
+    		
+    	}
+    }
+    @SuppressLint("NewApi")
+	public static class DetailsFragment extends Fragment {
+    	
+    }
+}
+
+
+

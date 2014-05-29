1efffba6320535bedba8f530b9c48166e936a5fb
hjeong <Felinus.jeong@windriver.com>
2014-05-29 18:36:01 +0900

    ToDolist

--- FragmentLayout/src/com/example/fragmentlayout/FragmentLayout.java
+++ FragmentLayout/src/com/example/fragmentlayout/FragmentLayout.java
@@ -2,10 +2,22 @@ package com.example.fragmentlayout;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.app.ListFragment;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
 
 @SuppressLint("NewApi")
 public class FragmentLayout extends Activity
@@ -16,9 +28,10 @@ public class FragmentLayout extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_layout);
+
     }
     
-    @SuppressLint("NewApi")
+
 	public static class DetailsActivity extends Activity {
     	protected void onCreate(Bundle savedInstanceState) {
     		super.onCreate(savedInstanceState);
@@ -43,13 +56,89 @@ public class FragmentLayout extends Activity
     	public void onActivityCreated(Bundle savedInstatnceState) {
     		super.onActivityCreated(savedInstatnceState);
     		
-    		setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.sample_list_item))
+    		setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_activated_1,Shakespeare.TITLES));
+    		View detailsFrame = getActivity().findViewById(R.id.details);
+    		mDualPane = detailsFrame !=null && detailsFrame.getVisibility() == View.VISIBLE;
     		
+    		if(savedInstatnceState != null) {
+    			mCurCheckPosition = savedInstatnceState.getInt("curChoice", 0);
+    		}
+    		
+    		if(mDualPane) {
+    			getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+    			showDetails(mCurCheckPosition);
+    		}
     	}
+    	
+    	public void onSavedInstanceState(Bundle outState) {
+    		super.onSaveInstanceState(outState);
+    		outState.putInt("curChoice", mCurCheckPosition);
+    	}
+    	
+    	public void onListItemClick(ListView l, View v , int position, long id) {
+    		showDetails(position);
+    	}
+    	
+    	void showDetails(int index) {
+    		mCurCheckPosition = index;
+    		
+    		if(mDualPane) {
+    			getListView().setItemChecked(index, true);
+    			
+    			DetailsFragment details = (DetailsFragment) getFragmentManager().findFragmentById(R.id.details);
+    			if(details == null || details.getShownIndex() !=index) {
+    				FragmentTransaction ft = getFragmentManager().beginTransaction();
+    				
+    				if(index == 0) {
+    					ft.replace(R.id.details, details);
+    				} else {
+    					ft.replace(R.id.a_item, details);
+    				}
+    				ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+    				ft.commit();
+    				
+    			} else {
+    				Intent intent = new Intent();
+    				intent.setClass(getActivity(), DetailsActivity.class);
+    				intent.putExtra("index", index);
+    				startActivity(intent);
+    			}
+    			
+    		}
+    	} 	
+
     }
     @SuppressLint("NewApi")
 	public static class DetailsFragment extends Fragment {
     	
+    	public static DetailsFragment newInstance(int index) {
+    		DetailsFragment f = new DetailsFragment();
+    		
+    		Bundle args = new Bundle();
+    		args.putInt("index", index);
+    		f.setArguments(args);
+    		
+    		return f;
+    	}
+    	public int getShownIndex() {
+    		return getArguments().getInt("index",0);
+    	}
+    	
+    	public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+    			Bundle savedInstanceState) {
+    		if(container == null) {
+    			return null;
+    		}
+    		
+    		ScrollView scroller = new ScrollView(getActivity());
+    		TextView text = new TextView(getActivity());
+    		int padding = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, 
+    				getActivity().getResources().getDisplayMetrics());
+    		text.setPadding(padding, padding, padding,padding);
+    		scroller.addView(text);
+    		text.setText(Shakespeare.DIALOGUE[getShownIndex()]);
+    		return scroller;
+    	}
     }
 }
 
--- ToDolist/src/com/example/todolist/ToDolist.java
+++ ToDolist/src/com/example/todolist/ToDolist.java
@@ -1,84 +1,94 @@
 package com.example.todolist;
 
 
+import java.util.ArrayList;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnKeyListener;
+import android.view.View.OnTouchListener;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+import android.widget.Toast;
 
 public class ToDolist extends Activity
 {
-	int MAX=100;
-	String[] todolist = new String[MAX]; // null 로 초기화 되서 문제 
-  
+	ArrayList<String> todolist = new ArrayList<String>(); // null 로 초기화 되서 문제 
+	EditText editText;
 	private ArrayAdapter<String> myAdapter;
 	private ListView list;
-	int i=0;	
+	int i=0;
+	boolean check= false;
 	/** Called when the activity is first created. */
+
 
 	@SuppressLint("NewApi")
 	@Override
     public void onCreate(Bundle savedInstanceState)
-    {
+    {	       
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        for(int i=0; i<MAX; i++)
-        	todolist[i]="";
-        final EditText editText = (EditText) findViewById(R.id.edit);
-    //    InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
-    //   inputMethodManager.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT);
-   /**/     
-        editText.setOnKeyListener(new OnKeyListener()
-        {
-            public boolean onKey(View v, int keyCode, KeyEvent event)
-            {
-                if (event.getAction() == KeyEvent.ACTION_DOWN)
-                {
-                	Log.e("KeyEvent.ACTION_DOWN","KeyEvent.ACTION_DOWN");
-                    switch (keyCode)
-                    {
-                        case KeyEvent.KEYCODE_ENTER:
-                        		Log.e("Keycode_ender","Keycode_ender");
-                        		todolist[i]= editText.getText().toString();
-                        		i++;
-                        		list.setAdapter(myAdapter);
-                            return true;
-                        default:
-                            break;
-                    }
-                } else{
-                	Log.e("KeyEvent.ACTION_else",""+keyCode);
-                }
-                
-                return false;
-            }
-		
-        });
-       
-        /**/
-        myAdapter = new ArrayAdapter<String>(this, 
-        		android.R.layout.simple_list_item_single_choice, 
-        		todolist);
-        
-        list= (ListView)findViewById(R.id.listview);
-        list.setAdapter(myAdapter);
-        list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);    
-    
-    }
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		// TODO Auto-generated method stub
-		Log.e("KeyEvent.ACTION_DOWN","KeyEvent.ACTION_DOWN");
-		return super.onKeyDown(keyCode, event);
-	}  
-	
+  	  myAdapter = new ArrayAdapter<String>(this, 
+      		android.R.layout.simple_list_item_single_choice, 
+      		todolist);
+      
+      list= (ListView)findViewById(R.id.listview);
+      list.setAdapter(myAdapter);
+      list.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+      editText = (EditText) findViewById(R.id.edit);
+      editText.setInputType(0);
+  
+      
+   //  InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+  //  imm.showSoftInput(editText, InputMethodManager.SHOW_FORCED);
 
+     
+      /*
+         InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+    inputMethodManager.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT);
+     */     
+        editText.setOnEditorActionListener(new OnEditorActionListener(){
+
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				// TODO Auto-generated method stub
+				 if(actionId==EditorInfo.IME_ACTION_DONE){
+					 Log.e("IME_ACTION_DONE","IME_ACTION_DONE");
+					 todolist.add(0, editText.getText().toString());
+					 editText.setText(R.string.add_a_new_task);
+					 editText.setInputType(0);
+					 check=false;
+				 }
+				return false;
+			}
+        	
+        });
+        
+        editText.setOnTouchListener(new OnTouchListener(){
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				// TODO Auto-generated method stub
+				Log.e("OnTouchListener","OnTouchListener");
+				editText.setInputType(1);
+				
+				if(check == false){
+				editText.setText("");
+				check = true;
+				}
+				return false;
+			}
+    });
+    }
 }
